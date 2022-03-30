58f1d14be5322e75f01f31092f62aa46c94fa242
Kwanda Silekwa <kwanda9700@gmail.com>
2022-03-30 17:30:58 +0200

    Update Word_occurence.php
    update

--- Word_occurence.php
+++ Word_occurence.php
@@ -6,6 +6,8 @@ function wordOccurence($str, $word) {
     $word_occurence = substr_count($str_toLower, $word_toLower);
     return $word_occurence;
 }
-num = wordOccurence("Hello I looking for how many word occurences that are in this. Hello I looking for how many word occurences that are in this", "this");
-
+$str = "Hello This is a test by looking for how many word occurences that are in this. Hello I looking for how many word occurences that are in this";
+$word = "This";
+$num = wordOccurence($str, $word);
+echo "The word " . $word . " occures " . $num . " times"
 ?>
